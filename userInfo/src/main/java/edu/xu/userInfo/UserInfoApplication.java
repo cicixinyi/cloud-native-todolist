9a417b87a9dfa5a91cffd41f77f2fdd7136beff5
Xinyi Xu <90807887+cicixinyi@users.noreply.github.com>
2023-05-01 01:14:08 -0400

    Update UserInfoApplication.java

--- userInfo/src/main/java/edu/xu/userInfo/UserInfoApplication.java
+++ userInfo/src/main/java/edu/xu/userInfo/UserInfoApplication.java
@@ -1,9 +1,12 @@
 package edu.xu.userInfo;
 
+import java.util.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
+@RestController
 public class UserInfoApplication {
 
 	public static void main(String[] args) {
