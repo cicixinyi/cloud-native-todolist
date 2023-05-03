4e990469b8d30bec5d8d92c7ae52de834b4a906d
Xinyi Xu <xxu@trincoll.edu>
2023-05-03 16:15:19 -0400

    userInfo microservice

--- userInfo/src/main/java/edu/xu/userInfo/UserInfoApplication.java
+++ userInfo/src/main/java/edu/xu/userInfo/UserInfoApplication.java
@@ -1,16 +1,17 @@
 package edu.xu.userInfo;
 
 import java.util.*;
+import org.springframework.beans.factory.annotation.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
 @RestController
 public class UserInfoApplication {
 
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(UserInfoApplication.class, args);
 	}
-
 }
