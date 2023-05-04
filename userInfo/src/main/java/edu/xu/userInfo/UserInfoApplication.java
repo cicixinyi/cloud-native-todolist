e8ace359c46b6d620914d449ff6543c6cdf9690b
Xinyi Xu <xxu@trincoll.edu>
2023-05-04 00:11:53 -0400

    no

--- userInfo/src/main/java/edu/xu/userInfo/UserInfoApplication.java
+++ userInfo/src/main/java/edu/xu/userInfo/UserInfoApplication.java
@@ -1,9 +1,9 @@
 package edu.xu.userInfo;
 
 import java.util.*;
-import org.springframework.beans.factory.annotation.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.beans.factory.annotation.*;
 
 @SpringBootApplication
 @RestController
