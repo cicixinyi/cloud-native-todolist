a823dea0389352b709768c821d4ee4f15f293f71
Xinyi Xu <90807887+cicixinyi@users.noreply.github.com>
2023-05-08 06:43:49 +0100

    Add files via upload

---
+++ tasks/src/test/java/edu/xu/tasks/TasksApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.xu.tasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
